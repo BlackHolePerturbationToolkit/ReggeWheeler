c36e598be0b81ecfb60c150f8ee7415d7ad4354f
Barry Wardell <barry.wardell@gmail.com>
2019-03-21 16:57:53 +0000

    Partially implement ReggeWheelerSource

--- ReggeWheelerSource.m
+++ ReggeWheelerSource.m
@@ -1,7 +1,22 @@
 BeginPackage["ReggeWheeler`ReggeWheelerSource`"];
 
+ReggeWheelerSourceObject::usage = "ReggeWheelerSourceObject[assoc] an object which contains a Regge Wheeler source."
+
+ReggeWheelerPointParticleSource::usage = "ReggeWheelerPointParticleSource[s, orbit] Point particle source for the Regge Wheeler equation."
 
 Begin["`Private`"];
 
-End[]
+
+ReggeWheelerPointParticleSource[variable_, orbit_] :=
+ Module[{assoc, source},
+  assoc = <| "Variable" -> variable, "sourceType" -> "PointParticle" |>;
+  ReggeWheelerSourceObject[assoc]
+];
+
+Format[ReggeWheelerSourceObject[assoc_]] := "ReggeWheelerSourceObject[<<>>]";
+
+ReggeWheelerSourceObject[assoc_][string_] := assoc[string];
+
+
+End[];
 EndPackage[];
