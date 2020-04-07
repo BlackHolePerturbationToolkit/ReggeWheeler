029f976dc010027e27e1eecb7c66e1c6f9e8d0e5
Barry Wardell <barry.wardell@gmail.com>
2020-04-08 00:21:29 +0100

    Genereate a Message when asked to compute a mode for a!=0
    Also remove support for passing a source object to a
    ReggeWheelerPointParticleMode since it is not currently used.

--- ReggeWheelerMode.m
+++ ReggeWheelerMode.m
@@ -31,6 +31,13 @@ EnergyFlux::usage = "EnergyFlux[mode] computes the flux of energy radiated in th
 
 
 (* ::Subsection::Closed:: *)
+(*Error Messages*)
+
+
+ReggeWheelerPointParticleMode::nospin = "Regge-Wheeler perturbations are only for Schwarzschild black holes but spin `1` is not zero."
+
+
+(* ::Subsection::Closed:: *)
 (*Begin Private section*)
 
 
@@ -41,26 +48,20 @@ Begin["`Private`"];
 (*ReggeWheelerPointParticleMode*)
 
 
-(*defined currently for circular orbits*)
 ReggeWheelerPointParticleMode[s_Integer, l_Integer, m_Integer, n_Integer, orbit_KerrGeoOrbitFunction] :=
-	Module[{source},
-	
-		If[!PossibleZeroQ[orbit["a"]], Print["Regge-Wheeler perturbations are only for Schwarzschild (a=0) black holes"]; Return[];];
-	
-		source = ReggeWheelerPointParticleSource[s, l, m, orbit];
-		Return[ReggeWheelerPointParticleMode[s, l, m, n, orbit, source]];
+ Module[{source, assoc, R, S, \[Omega], \[CapitalOmega]r, \[CapitalOmega]\[Phi], \[CapitalOmega]\[Theta], Z},
+  If[orbit["a"] != 0,
+    Message[ReggeWheelerPointParticleMode::nospin, orbit["a"]];
+    Return[$Failed];
 	];
 
-
-ReggeWheelerPointParticleMode[s_Integer, l_Integer, m_Integer, n_Integer, orbit_KerrGeoOrbitFunction, source_ReggeWheelerSourceObject] :=
- Module[{assoc, R, S, \[Omega], \[CapitalOmega]r, \[CapitalOmega]\[Phi], \[CapitalOmega]\[Theta], Z},
   (*{\[CapitalOmega]r, \[CapitalOmega]\[Theta], \[CapitalOmega]\[Phi]} = orbit["Frequencies"];*) (*This gives Mino frequencies, need BL frequencies*)
   {\[CapitalOmega]r, \[CapitalOmega]\[Theta], \[CapitalOmega]\[Phi]} = Values[KerrGeoFrequencies[orbit["a"], orbit["p"], orbit["e"], orbit["Inclination"]]];
   \[Omega] = m \[CapitalOmega]\[Phi] + n \[CapitalOmega]r;
 
-  R = ReggeWheelerRadial[s, l, \[Omega]];
-  
+  source = ReggeWheelerPointParticleSource[s, l, m, orbit];
 
+  R = ReggeWheelerRadial[s, l, \[Omega]];
   Z = ReggeWheeler`ConvolveSource`Private`ConvolveSource[R, source];
 
   assoc = <| "s" -> s, 
